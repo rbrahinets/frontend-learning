b7d1d60dad791cbff3e70d57003235ff21fa3a76
Rostyslav <rostyslav.brahinets@gmail.com>
2023-07-13 00:22:57 +0300

    Update App component

--- 03-weather-app-react-ts/src/App.tsx
+++ 03-weather-app-react-ts/src/App.tsx
@@ -1,7 +1,28 @@
-const App = () => {
+import useForecast from './hooks/useForecast';
+import Search from './components/Search';
+import Forecast from './components/Forecast';
+
+const App = (): JSX.Element => {
+    const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+        useForecast();
+
     return (
-        <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-            Hello world
+        <main
+            className="flex justify-center items-center
+        bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400
+        h-[100vh] w-full"
+        >
+            {forecast ? (
+                <Forecast data={forecast} />
+            ) : (
+                <Search
+                    term={term}
+                    options={options}
+                    onInputChange={onInputChange}
+                    onOptionSelect={onOptionSelect}
+                    onSubmit={onSubmit}
+                />
+            )}
         </main>
     );
 };
