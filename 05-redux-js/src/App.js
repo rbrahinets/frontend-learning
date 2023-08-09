2a8c88c8cbf51099d613fb1e5dde68079b45788a
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-09 15:52:53 +0300

    Update component App

--- 05-redux-js/src/App.js
+++ 05-redux-js/src/App.js
@@ -1,30 +1,31 @@
-import './App.css';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { actions } from './store';
 
 const App = () => {
     const counter = useSelector((state) => state.counter);
     const dispatch = useDispatch();
 
     const increment = () => {
-        dispatch({ type: 'INC' });
+        dispatch(actions.increment());
     };
 
     const decrement = () => {
-        dispatch({ type: 'DEC' });
+        dispatch(actions.decrement());
     };
 
     const addBy = () => {
-        dispatch({ type: 'ADD', payload: 10 });
+        dispatch(actions.addBy(10));
     };
 
     return (
-        <div className="App">
+        <React.Fragment>
             <h1>Counter App</h1>
             <h2>{counter}</h2>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
             <button onClick={addBy}>Add Value</button>
-        </div>
+        </React.Fragment>
     );
 };
 
