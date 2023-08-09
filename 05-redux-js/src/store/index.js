e5766f86c0b88e7d3c238b566104f8047e18ff09
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-09 15:52:22 +0300

    Update store with toolkit

--- 05-redux-js/src/store/index.js
+++ 05-redux-js/src/store/index.js
@@ -1,17 +1,25 @@
-import { legacy_createStore as createStore } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const reducerFn = (state = { counter: 0 }, action) => {
-    if (action.type === 'INC') {
-        return { counter: state.counter + 1 };
-    } else if (action.type === 'DEC') {
-        return { counter: state.counter - 1 };
-    } else if (action.type === 'ADD') {
-        return { counter: state.counter + action.payload };
-    }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { counter: 0 },
+    reducers: {
+        increment(state, action) {
+            state.counter++;
+        },
+        decrement(state, action) {
+            state.counter--;
+        },
+        addBy(state, action) {
+            state.counter += action.payload;
+        },
+    },
+});
 
-    return state;
-};
+export const actions = counterSlice.actions;
 
-const store = createStore(reducerFn);
+const store = configureStore({
+    reducer: counterSlice.reducer,
+});
 
 export default store;
