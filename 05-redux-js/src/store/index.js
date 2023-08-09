81ef294567c3b63a26cb6c29726eac8991185e60
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-09 15:31:34 +0300

    Add actions to store

--- 05-redux-js/src/store/index.js
+++ 05-redux-js/src/store/index.js
@@ -1,6 +1,14 @@
 import { legacy_createStore as createStore } from 'redux';
 
 const reducerFn = (state = { counter: 0 }, action) => {
+    if (action.type === 'INC') {
+        return { counter: state.counter + 1 };
+    } else if (action.type === 'DEC') {
+        return { counter: state.counter - 1 };
+    } else if (action.type === 'ADD') {
+        return { counter: state.counter + action.payload };
+    }
+
     return state;
 };
 
