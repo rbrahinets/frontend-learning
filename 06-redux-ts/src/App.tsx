a672c2f09a62794fa75988906b587c9b81f7eaa6
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-11 14:23:06 +0300

    Add title for app

--- 06-redux-ts/src/App.tsx
+++ 06-redux-ts/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <span className="heading">Taskify</span>
+        </div>
+    );
 };
 
 export default App;
