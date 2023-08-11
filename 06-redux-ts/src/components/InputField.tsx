a13d4f09066758064b6396d674a8d1001619d8e1
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-11 14:54:43 +0300

    Add ref for input

--- 06-redux-ts/src/components/InputField.tsx
+++ 06-redux-ts/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
@@ -8,10 +8,19 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return (
-        <form className="input" onSubmit={handleAdd}>
+        <form
+            className="input"
+            onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.blur();
+            }}
+        >
             <input
                 className="input__box"
+                ref={inputRef}
                 type="input"
                 value={todo}
                 onChange={(e) => setTodo(e.target.value)}
