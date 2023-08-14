9c0d668c1cc5d69b1ce71999db22a72f09c74921
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-14 16:59:54 +0300

    Update App component

--- 08-mobx-ts/src/App.tsx
+++ 08-mobx-ts/src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './App.css';
+import Athlete from './Athlete';
+import { TeamStoreProvider } from './TeamStore';
+import Rosters from './Rosters';
+import MoneyForm from './MoneyForm';
+
+const lebronJames = new Athlete('Lebron James', 37, 5_000_000);
+const stephCurry = new Athlete('Steph Curry', 34, 4_000_000);
+
+const getPlayersFromBackend = (): Athlete[] => {
+    return [lebronJames, stephCurry];
+};
 
 const App = () => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <TeamStoreProvider players={getPlayersFromBackend()}>
+                <Rosters />
+                <MoneyForm />
+            </TeamStoreProvider>
+        </React.Fragment>
+    );
 };
 
 export default App;
