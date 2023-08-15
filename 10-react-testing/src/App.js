2b4df163421ac6a555bc07142614d7358892e38d
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-15 20:07:49 +0300

    Add Header to App

--- 10-react-testing/src/App.js
+++ 10-react-testing/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from './components/header';
+import './app.scss';
 
 const App = () => {
-    return <React.Fragment></React.Fragment>;
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    );
 };
 
 export default App;
