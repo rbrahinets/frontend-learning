566db33a24b78678f9b32cc840769db52737b650
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-15 20:46:58 +0300

    Add attributes for tags

--- 10-react-testing/src/components/header/index.js
+++ 10-react-testing/src/components/header/index.js
@@ -3,10 +3,10 @@ import Logo from '../../assets/graphics/logo.png';
 
 const Header = (props) => {
     return (
-        <header>
+        <header data-test="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Logo" />
+                    <img data-test="logoImage" src={Logo} alt="Logo" />
                 </div>
             </div>
         </header>
