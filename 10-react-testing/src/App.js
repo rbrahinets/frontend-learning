2bd4dd0096546da26a1920c37752b896de0082cc
Rostyslav <rostyslav.brahinets@gmail.com>
2023-08-16 17:19:34 +0300

    Add Headline to App

--- 10-react-testing/src/App.js
+++ 10-react-testing/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './components/header';
 import './app.scss';
+import Headline from './components/headlines';
 
 const App = () => {
     return (
         <React.Fragment>
             <Header />
+            <section className="main">
+                <Headline
+                    header="Posts"
+                    desc="Click the button to render posts"
+                />
+            </section>
         </React.Fragment>
     );
 };
