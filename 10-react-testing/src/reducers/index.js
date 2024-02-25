3f92bd37f40d7b827a61f717fb63ce8208a2f285
rbrahinets <rostyslav.brahinets@gmail.com>
2024-02-25 20:21:28 +0200

    Add tests for posts reducer

--- 10-react-testing/src/reducers/index.js
+++ 10-react-testing/src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
 import success from './successReducer';
+import {posts} from './posts/reducer'
 
 export default combineReducers({
-    success
+    success,
+    posts,
 });
