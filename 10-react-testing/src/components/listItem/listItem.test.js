8b318b48d95c4507b2ed50a2dc611b07c8c63694
rbrahinets <rostyslav.brahinets@gmail.com>
2024-02-27 18:01:13 +0200

    Add tests for list item

---
+++ 10-react-testing/src/components/listItem/listItem.test.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {checkProps, findByTestAttr} from '../../../utils';
+import ListItem from './index';
+
+describe('ListItem Component', () => {
+    describe('Checking PropTypes', () => {
+        it('Should NOT throw a warning', () => {
+            const expectedProps = {
+                title: 'Example Title',
+                emitEvent: 'Some text',
+            };
+
+            const propsError = checkProps(ListItem, expectedProps);
+
+            expect(propsError).toBeUndefined();
+        });
+    });
+});
+
+describe('Component Renders', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        const props = {
+            title: 'Example Title',
+            emitEvent: 'Some text',
+        };
+
+        wrapper = shallow(<ListItem {...props}/>)
+    });
+
+    it('Should renders without error', () => {
+        const component = findByTestAttr(wrapper, 'listItemComponent');
+        expect(component.length).toBe(1);
+    });
+
+    it('Should render a title', () => {
+        const title = findByTestAttr(wrapper, 'componentTitle');
+        expect(title.length).toBe(1);
+    });
+
+    it('Should render a desc', () => {
+        const desc = findByTestAttr(wrapper, 'componentDesc');
+        expect(desc.length).toBe(1);
+    });
+
+    describe('Should NOT render', () => {
+        let wrapper;
+
+        beforeEach(() => {
+            const props = {
+                desc: 'Some text',
+            };
+
+            wrapper = shallow(<ListItem {...props}/>)
+        });
+
+        it('Component is not rendered', () => {
+            const component = findByTestAttr(wrapper, 'listItemComponent');
+            expect(component.length).toBe(0);
+        });
+    });
+});
